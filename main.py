bbc8b4ff29edd1f44cad3324cae1f700a60f0697
zatguy79 <50187413+zatguy79@users.noreply.github.com>
2026-01-14 21:40:43 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 # testing the oTa code
 print("Hiii huys")
+
+import machine
+import time
+
+led_pin = machine.Pin(15, machine.Pin.OUT)
+
+while True:
+    
+    led_pin.value(1)
+    time.sleep(1)
+    led_pin.value(0)
+    time.sleep(1)
